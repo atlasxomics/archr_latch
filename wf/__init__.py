56b52f2c03113b8ca0c1b967e9c679f77cd548bb
Hannah Le <hannah.lgbhan@gmail.com>
2023-09-25 20:10:48 +0000

    registry additions

--- wf/__init__.py
+++ wf/__init__.py
@@ -26,19 +26,7 @@ from latch.types import (
 )
 
 import wf.lims as lims
-
-@dataclass_json
-@dataclass
-class Run:
-    run_id: str
-    fragments_file: LatchFile
-    condition: str = 'None'
-    spatial_dir: LatchDir = LatchDir(
-        'latch:///spatials/demo/spatial/'
-    )
-    positions_file: LatchFile = LatchFile(
-        'latch:///spatials/demo/spatial/tissue_positions_list.csv'
-    )
+from wf.registry import Run, upload_to_registry
 
 class Genome(Enum):
     mm10 = 'mm10'
@@ -169,7 +157,8 @@ metadata = LatchMetadata(
                          run_id and fragments.tsv file; optional: condition, \
                          tissue position file for filtering on/off tissue, \
                          spatial folder for SpatialDimPlot.',
-            batch_table_column=True, 
+            batch_table_column=True,
+            samplesheet=True
         ),
         'project_name' : LatchParameter(
             display_name='project name',
@@ -244,7 +233,11 @@ metadata = LatchMetadata(
             description='minDist parameter from addUMAP function.',
             batch_table_column=True,
             hidden=True
-        ),                 
+        ),
+        'table_id': LatchParameter(
+            display_name='Registry Table ID',
+            description='The runs will be updated in Registry with its corresponding condition, spatial directory, condition, and location of the optimized output archR project.'
+        )                  
     },
     tags=[],
 )
@@ -254,6 +247,7 @@ def archr_workflow(
     runs: List[Run],
     genome: Genome,
     project_name: str,
+    table_id: str = "761",
     upload: bool=False,
     tile_size: int=5000,
     min_TSS: float=2.0,
@@ -435,6 +429,12 @@ def archr_workflow(
         umap_mindist=umap_mindist
     )
 
+    upload_to_registry(
+        runs=runs,
+        archr_project=results_dir,
+        table_id=table_id
+    )
+
     return lims_task(results_dir=results_dir, upload=upload)
 
 LaunchPlan(
@@ -452,6 +452,7 @@ LaunchPlan(
         ],
     'project_name' : 'demo',
     'genome' : Genome.hg38,
-    'upload' : False
+    'upload' : False,
+    'table_id': '761'
     },
 )
